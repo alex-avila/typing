a970dedab7bc47e04ddbbb15fd854e5d1bef1882
alex-avila <alex.avilx@icloud.com>
2018-08-16 22:23:17 -0600

    Removed example components

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -4,12 +4,6 @@
 var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
 var Text$ReactTemplate = require("./Text.bs.js");
-var Component1$ReactTemplate = require("./Component1.bs.js");
-var Component2$ReactTemplate = require("./Component2.bs.js");
-
-ReactDOMRe.renderToElementWithId(ReasonReact.element(undefined, undefined, Component1$ReactTemplate.make("Hello!", /* array */[])), "index1");
-
-ReactDOMRe.renderToElementWithId(ReasonReact.element(undefined, undefined, Component2$ReactTemplate.make("Hello!", /* array */[])), "index2");
 
 var paragraph = "Hello this is absolutely a paragraph";
 
